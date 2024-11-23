2e0c729abeff354a4b03e310011cc18e35c4516b
Luciferhub44 <moltenwastelands@gmail.com>
2024-11-23 06:09:58 -0500

    wagmi fixed

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,17 +1,13 @@
-import { createConfig, configureChains } from 'wagmi';
+import { createConfig, http } from 'wagmi';
 import { mainnet, goerli } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
 import { DefaultChainID } from './constants';
 
-const { chains, publicClient } = configureChains(
-  [DefaultChainID === 1 ? mainnet : goerli],
-  [publicProvider()]
-);
-
 const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  chains,
+  chains: [DefaultChainID === 1 ? mainnet : goerli],
+  transports: {
+    [mainnet.id]: http(),
+    [goerli.id]: http(),
+  },
 });
 
 export default config; 
